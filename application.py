c5e875adeb9271563fc06aa8ac2bc204642a7647
rajuu9825 <rajuu9825@gmail.com>
2024-12-15 17:26:54 +0530

    first commit

---
+++ application.py
@@ -0,0 +1,44 @@
+import pickle
+from flask import Flask,request,jsonify,render_template
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+
+application = Flask(__name__)
+app = application
+
+## import ridge regressor and stanard scaler pickle
+ridge_model = pickle.load(open('Model/ridge.pkl','rb'))
+standard_scaler = pickle.load(open('Model/scaler.pkl','rb')) 
+
+
+@app.route("/")  # home page
+def index():
+    return render_template('index.html') # it will try to open index.html from the templates folder
+
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():
+    if request.method == "POST":
+        Temperature = float(request.form.get('Temperature'))
+        RH = float(request.form.get('RH'))
+        Ws = float(request.form.get('Ws'))
+        Rain =float(request.form.get('Rain'))
+        FFMC = float(request.form.get('FFMC'))
+        DMC = float(request.form.get('DMC'))
+        ISI = float(request.form.get('ISI'))
+        Classes = float(request.form.get('Classes'))
+        Region = float(request.form.get('Region'))
+        
+        new_data_scaled = standard_scaler.transform([[Temperature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])
+        result = ridge_model.predict(new_data_scaled)
+        return render_template('home.html',results = result[0]) # why result[0] because it will be in the form of list, so will get only one data in list thats why.
+
+
+    else: 
+        return render_template('home.html')
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
